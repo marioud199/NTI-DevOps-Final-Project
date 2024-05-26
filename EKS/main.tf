1a277374736e655c90faec1364f8a74e15428309
marioud199 <77901103+marioud199@users.noreply.github.com>
2024-05-26 04:33:56 +0300

    Add files via upload

---
+++ EKS/main.tf
@@ -0,0 +1,192 @@
+# resource "aws_eks_cluster" "marioud_cluster" {
+#   name     = var.eks_name
+#   role_arn = aws_iam_role.eks_cluster_role.arn
+
+#   vpc_config {
+#     subnet_ids = concat(var.pri_subnet_ids, var.pub_subnet_ids)
+#   }
+# }
+
+# resource "aws_iam_role" "eks_cluster_role" {
+#   name = "eks-cluster"
+
+#   assume_role_policy = jsonencode({
+#     Version   = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = {
+#           Service = "eks.amazonaws.com"
+#         },
+#         Action    = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
+
+# # resource "aws_iam_policy" "ecr_policy" {
+# #   name        = "ecr_policy"
+# #   description = "Allow ECR actions"
+# #   policy      = jsonencode({
+# #     Version   = "2012-10-17",
+# #     Statement = [
+# #       {
+# #         Effect   = "Allow",
+# #         Action   = [
+# #           "ecr:GetDownloadUrlForLayer",
+# #           "ecr:BatchGetImage",
+# #           "ecr:BatchCheckLayerAvailability",
+# #           "ecr:GetAuthorizationToken"
+# #         ],
+# #         Resource = "*"
+# #       }
+# #     ]
+# #   })
+# # }
+
+# # resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+# #   policy_arn = aws_iam_policy.ecr_policy.arn
+# #   role       = aws_iam_role.eks_cluster_role.id
+# # }
+
+# resource "aws_eks_node_group" "private-nodes" {
+#   cluster_name = aws_eks_cluster.marioud_cluster.id
+#   node_group_name = "private-nodes"
+#   node_role_arn = aws_iam_role.nodes.arn
+#   subnet_ids = var.pri_subnet_ids
+#   capacity_type  = "ON_DEMAND"
+#   instance_types = ["t2.medium"]
+
+#   scaling_config {
+#     desired_size = var.desired
+#     max_size     = var.max_size
+#     min_size     = var.min_size
+#   }
+
+#   update_config {
+#     max_unavailable = 1
+#   }
+
+#   labels = {
+#     role = "general"
+#   }
+# }
+
+# resource "aws_iam_role" "nodes" {
+#   name = "eks-node-group-nodes"
+
+#   assume_role_policy = jsonencode({
+#     Statement = [{
+#       Action    = "sts:AssumeRole",
+#       Effect    = "Allow",
+#       Principal = {
+#         Service = "ec2.amazonaws.com"
+#       }
+#     }],
+#     Version   = "2012-10-17"
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role       = aws_iam_role.nodes.id
+# }
+
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role       = aws_iam_role.nodes.id
+# }
+
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role       = aws_iam_role.nodes.id
+# }
+
+resource "aws_eks_cluster" "marioud_cluster" {
+    name = var.eks_name
+    role_arn = aws_iam_role.eks_cluster_role.arn
+    vpc_config {
+       subnet_ids = concat(var.pri_subnet_ids,var.pub_subnet_ids)
+      
+  }
+      
+    }
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.id
+}
+
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name = aws_eks_cluster.marioud_cluster.id
+  node_group_name = "private-nodes"
+  node_role_arn = aws_iam_role.nodes.arn
+  subnet_ids = var.pri_subnet_ids
+   capacity_type  = "ON_DEMAND"
+   instance_types = ["t2.medium"]
+
+   scaling_config {
+    desired_size = var.desired
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+   update_config {
+    max_unavailable = 1
+  }
+
+
+
+
+  labels = {
+    role = "general"
+  }
+
+}
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.id
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.id
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.id
+}
---
+++ Jenkinsfile
@@ -0,0 +1,54 @@
+pipeline {
+    agent any 
+
+    environment {
+        REPO_SERVER = "730335618715.dkr.ecr.eu-west-1.amazonaws.com/backend_repo"
+        REPO_NAME_BACKEND = "${REPO_SERVER}:backend"
+        REPO_NAME_FRONTEND = "${REPO_SERVER}:frontend"
+        IMAGE_VERSION = "${BUILD_NUMBER}"
+        AWS_REGION = 'eu-west-1'
+    }
+
+    stages {
+
+        stage("build image") {
+            steps {
+                script {
+                    echo "building docker images ..."
+                   {
+                        sh "aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URL}"
+                        sh "docker build app/backend/. -t ${REPO_NAME_BACKEND}-${IMAGE_VERSION}"
+                        sh "docker push ${REPO_NAME_BACKEND}-${IMAGE_VERSION}"
+                        sh "docker build app/frontend/. -t ${REPO_NAME_FRONTEND}-${IMAGE_VERSION}"
+                        sh "docker push ${REPO_NAME_FRONTEND}-${IMAGE_VERSION}"
+                    }
+                }
+            }
+        }
+
+        stage("change image version") {
+            steps {
+                script {
+                    echo "change image version .."
+                    sh "sed -i \"s|image:.*|image: ${REPO_NAME_BACKEND}-${IMAGE_VERSION}|g\" k8s/back.yaml"
+                    sh "sed -i \"s|image:.*|image: ${REPO_NAME_FRONTEND}-${IMAGE_VERSION}|g\" k8s/front.yaml"
+                }
+            }
+        }
+
+        stage('Deploy to eks cluster') {
+            // environment {
+            //     AWS_ACCESS_KEY_ID = credentials("aws_access_key_id")
+            //     AWS_SECRET_ACCESS_KEY = credentials("aws_secret_access_key")
+            // }
+            steps {
+                echo 'Deploying to eks cluster ... '
+                withCredentials([file(credentialsId:'kubeconfig', variable:'KUBECONFIG')]){
+                    script{
+                        sh 'kubectl apply -f k8s'
+                    }
+                }
+            }
+        }
+    }
+}
